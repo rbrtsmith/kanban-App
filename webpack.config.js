92bcad05c45c14f1b182184b4f53639aa1a054ce
Robert Smith <rbrtsmith84@googlemail.com>
2015-08-16 15:11:33 +0200

    fix indent

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
 var path = require('path'),
-	HtmlwebpackPlugin = require('html-webpack-plugin'),
-	webpack = require('webpack'),
-	merge = require('webpack-merge'),
-	TARGET = process.env.TARGET,
-	ROOT_PATH = path.resolve(__dirname),
-	common;
+    HtmlwebpackPlugin = require('html-webpack-plugin'),
+    webpack = require('webpack'),
+    merge = require('webpack-merge'),
+    TARGET = process.env.TARGET,
+    ROOT_PATH = path.resolve(__dirname),
+    common;
 
 common = {
-	entry: path.resolve(ROOT_PATH, 'app/main'),
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	output : {
-		path: path.resolve(ROOT_PATH, 'build'),
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.css$/,
-				loaders: ['style', 'css'],
-				includes: path.resolve(ROOT_PATH, 'app')
-			}
-		]
-	},
-	devserver: {
-		colors: true,
-		historyApiFallback: true,
-		hot: true,
-		inline: true,
-		progress: true
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new HtmlwebpackPlugin({
-			title: 'Kanban app'
-		})
-	]
+    entry: path.resolve(ROOT_PATH, 'app/main'),
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output : {
+        path: path.resolve(ROOT_PATH, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                includes: path.resolve(ROOT_PATH, 'app')
+            }
+        ]
+    },
+    devserver: {
+        colors: true,
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlwebpackPlugin({
+            title: 'Kanban app'
+        })
+    ]
 };
 
 if(TARGET === 'dev') {
