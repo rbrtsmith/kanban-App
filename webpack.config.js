307e366a43ce3d0976ad2a47089583b6da73289b
Robert Smith <rbrtsmith84@googlemail.com>
2015-08-16 14:16:25 +0200

    add react

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ var path = require('path'),
 
 common = {
 	entry: path.resolve(ROOT_PATH, 'app/main'),
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
 	output : {
 		path: path.resolve(ROOT_PATH, 'build'),
 		filename: 'bundle.js'
@@ -36,18 +39,17 @@ common = {
 	]
 };
 
-if (TARGET === 'dev') {
-	module.exports = merge(common, {
-		devtool: 'eval',
-		devserver: {
-			colors: true,
-			historyApiFallback: true,
-			hot: true,
-			inline: true,
-			progress: true
-		},
-		plugins: [
-			new webpack.HotModuleReplacementPlugin()
-		]
-	});
+if(TARGET === 'dev') {
+  module.exports = merge(common, {
+    devtool: 'eval',
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['babel?stage=1'],
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    }
+  });
 }
