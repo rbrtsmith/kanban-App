a8a88abfa560ac800554c94067e65d457146c2b8
Robert Smith <rbrtsmith84@googlemail.com>
2015-08-16 16:27:51 +0200

    edit task, display in console

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
-import Note from './Note';
+import uuid from 'node-uuid';
+import Notes from './Notes';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn webpack'                
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        };
+        this.addItem = this.addItem.bind(this);
+        this.itemEdited = this.itemEdited.bind(this);
+    }
     render() {
-        return <Note />;
+        const notes = this.state.notes;
+        return (
+            <div>
+                <button onClick={this.addItem}>+</button>
+                <Notes items={notes} onEdit={this.itemEdited} />
+            </div>
+        );
+    }
+    addItem() {
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New task'
+            }])
+        });
+    }
+    itemEdited(noteId, task) {
+        console.log('item edited', noteId, task);
     }
 }
